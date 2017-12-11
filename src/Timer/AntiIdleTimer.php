874393fd2402a8447ec97adc1f0b5e4ebb067fc7
倪述荣 <xueron@xueron.com>
2017-12-11 11:50:22 +0800

    Use pid in logger message

--- src/Timer/AntiIdleTimer.php
+++ src/Timer/AntiIdleTimer.php
@@ -14,9 +14,7 @@ class AntiIdleTimer extends Timer
 {
     public function handle($id, array $params = [])
     {
-        $workerId = server()->getSwoole()->worker_id;
-        $workerType = server()->getSwoole()->taskworker ? 'TaskWorker' : 'Worker';
-
+        $pid = getmypid();
         $databasePool = $params[0];
         $maxRetry = $params[1];
         $time = microtime(1);
@@ -25,16 +23,16 @@ class AntiIdleTimer extends Timer
             while ($tryTimes < $maxRetry) {
                 try {
                     $info = $databasePool->getConnectionInfo($key);
-                    logger()->log(Logger::DEBUG, "[$workerType $workerId] [Database $key] [$time] AntiIdle: " . $info['server']);
+                    logger()->log(Logger::DEBUG, "[$pid] [Database $key] [$time] AntiIdle: " . $info['server']);
                     break;
                 } catch (\Exception $e) {
                     if (strpos($e->getMessage(), 'MySQL server has gone away') !== false) {
-                        logger()->log(Logger::ERROR, "[$workerType $workerId] [Database $key] Connection lost, try to reconnect, tryTimes $tryTimes");
+                        logger()->log(Logger::ERROR, "[$pid] [Database $key] Connection lost, try to reconnect, tryTimes $tryTimes");
                         $databasePool->reconnect($key);
                         $tryTimes ++;
                         continue;
                     }
-                    logger()->log(Logger::ERROR, "[$workerType $workerId] [Database $key] Quit on exception: " . $e->getMessage());
+                    logger()->log(Logger::ERROR, "[$pid] [Database $key] Quit on exception: " . $e->getMessage());
                     exit;
                 }
             }
